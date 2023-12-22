4f0363ac4e4d00511b2a5e4dbb7c5cbb909a426a
Caesar629 <1934407246@qq.com>
2023-12-22 22:16:45 +0800

    更改lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,3 +26,7 @@ require("lspconfig").clangd.setup {
   capabilities = capabilities,
 }
 
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', '<LEADER>-', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<LEADER>=', vim.diagnostic.goto_next)
