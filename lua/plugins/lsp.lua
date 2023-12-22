6260d4d3d3c625b4860200383419e5a4f0ea38dd
Caesar629 <1934407246@qq.com>
2023-12-22 22:34:27 +0800

    添加python补全

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
     "clangd",
+    "pyright",
   },
 })
 
@@ -23,6 +24,10 @@ require("lspconfig").lua_ls.setup {
 }
 
 require("lspconfig").clangd.setup {
+  capabilities = capabilities,
+}
+
+require("lspconfig").pyright.setup {
   capabilities = capabilities,
 }
 
