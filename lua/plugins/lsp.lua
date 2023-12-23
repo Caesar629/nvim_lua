9547953ae572a984b304a2245bfe8fc80474b81f
Caesar629 <1934407246@qq.com>
2023-12-23 18:08:21 +0800

    1

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,7 @@
-require("mason").setup({
-  ui = {
-      icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-      }
-  }
-})
-
-require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "lua_ls",
-    "clangd",
-    "pyright",
-  },
-})
-
 local lspconfig = require("lspconfig")
-lspconfig.pyright.setup{}
-lspconfig.clangd.setup{}
-lspconfig.lua_ls.setup{}
+lspconfig.pyright.setup {}
+lspconfig.clangd.setup {}
+lspconfig.lua_ls.setup {}
 
 vim.keymap.set('n', '<LEADER>=', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<LEADER>-', vim.diagnostic.goto_prev)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,11 +104,6 @@ _G.packer_plugins = {
     path = "/home/ws/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["conform.nvim"] = {
-    loaded = true,
-    path = "/home/ws/.local/share/nvim/site/pack/packer/start/conform.nvim",
-    url = "https://github.com/stevearc/conform.nvim"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/ws/.local/share/nvim/site/pack/packer/start/friendly-snippets",
